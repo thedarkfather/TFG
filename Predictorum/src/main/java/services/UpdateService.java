98826b69f7af314252de0f724023b7db1a926878
Miguel Ángel <migu126@hotmail.com>
2015-09-08 20:07:09 +0200

    No usado

--- Predictorum/src/main/java/services/UpdateService.java
+++ Predictorum/src/main/java/services/UpdateService.java
@@ -17,7 +17,6 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
-import antlr.debug.NewLineEvent;
 import domain.Game;
 import domain.League;
 import domain.Prediction;
